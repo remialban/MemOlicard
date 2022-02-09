5e1467524553b7f87a6ee81c424be690fca79dcc
Rémi ALBAN <remialbanperso@gmail.com>
2022-02-09 13:15:09 +0100

    [fix](list): turbolinks

--- assets/edit_list.js
+++ assets/edit_list.js
@@ -2,9 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./edit_list/App.jsx";
 
-var element = document.getElementById("list");
+document.addEventListener("turbolinks:load", function() {
+    var element = document.getElementById("list");
 
-ReactDOM.render(
-    <App id={element.getAttribute("data-id")} />,
-    element
-);    
+    ReactDOM.render(
+        <App id={element.getAttribute("data-id")} />,
+        element
+    );   
+});
