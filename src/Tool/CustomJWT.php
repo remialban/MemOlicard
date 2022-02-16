9373fba9c95524ff7c806676a05462dca2088c75
Rémi ALBAN <remialbanperso@gmail.com>
2022-02-16 13:40:39 +0100

    [feat](service): jwt

---
+++ src/Tool/CustomJWT.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Tool;
+
+use Firebase\JWT\JWT;
+use Firebase\JWT\Key;
+
+class CustomJWT
+{
+    private string $publicKey;
+
+    private \OpenSSLAsymmetricKey $privateKey;
+
+    public function __construct(string $privateKeyPath)
+    {
+        $this->privateKey = openssl_pkey_get_private(file_get_contents($privateKeyPath));
+        $this->publicKey = openssl_pkey_get_details($this->privateKey)["key"];        
+    }
+
+    public function generateToken($payload): string
+    {
+        return JWT::encode($payload, $this->privateKey, 'RS256');
+    }
+
+    public function getData(string $token)
+    {
+        $decoded = JWT::decode($token, new Key($this->publicKey, 'RS256'));        
+        $decoded_array = (array) $decoded;
+        return $decoded_array;
+    }
+}
