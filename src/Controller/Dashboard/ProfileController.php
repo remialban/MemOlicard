7a1416eae22b8b467d79fb14fab11cdf7ee5cd3b
Rémi ALBAN <remialbanperso@gmail.com>
2022-02-19 13:39:38 +0100

    [fix](search): 404 page if user doesn't exist

--- src/Controller/Dashboard/ProfileController.php
+++ src/Controller/Dashboard/ProfileController.php
@@ -10,6 +10,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class ProfileController extends AbstractController
 {
@@ -21,7 +22,7 @@ class ProfileController extends AbstractController
         ]);
         if (!$user)
         {
-            $this->createNotFoundException();
+            throw new NotFoundHttpException("This user doesn't exist.");
         }
         return $this->render("dashboard/user/view.html.twig", [
             'user' => $user,
