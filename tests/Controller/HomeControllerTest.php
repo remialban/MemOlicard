9ff18877703267d9492026a7b45aa3a19353035d
Rémi ALBAN <remialbanperso@gmail.com>
2022-03-04 21:22:01 +0100

    [test](home): add test for the home page if user isn't connected

---
+++ tests/Controller/HomeControllerTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class HomeControllerTest extends WebTestCase
+{
+    public function testHomePage(): void
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/');
+
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorTextContains('h1', 'Welcome to MemOlicard');
+    }
+}
