2be231129a17efaf26291ff2f63e8276f4275ba6
Rémi ALBAN <remialbanperso@gmail.com>
2022-03-05 15:55:02 +0100

    [test](controller): add test for the home controller

--- tests/Controller/HomeControllerTest.php
+++ tests/Controller/HomeControllerTest.php
@@ -2,6 +2,7 @@
 
 namespace App\Tests\Controller;
 
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class HomeControllerTest extends WebTestCase
@@ -13,5 +14,20 @@ class HomeControllerTest extends WebTestCase
 
         $this->assertResponseIsSuccessful();
         $this->assertSelectorTextContains('h1', 'Welcome to MemOlicard');
+    }
+
+    public function testHomePageIfUserIsLogin(): void
+    {
+        $client = static::createClient();
+
+        $userRepository = static::getContainer()->get(UserRepository::class);
+        $user = $userRepository->findOneBy([
+            "username" => "johndoe",
+        ]);
+        $client->loginUser($user);
+
+        $crawler = $client->request('GET', '/');
+
+        $this->assertResponseRedirects('/dashboard');
     }
 }
