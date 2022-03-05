afba9a8ff438ea125cb9910b0790cd557ea06642
Rémi ALBAN <remialbanperso@gmail.com>
2022-03-05 10:51:21 +0100

    [fix](assets): resolve some bugs

--- assets/app.js
+++ assets/app.js
@@ -1,6 +1,8 @@
 import "./bootstrap";
 
-require("./disable_turbolinks_for_the_forms").run();
-require("./edit_list").run();
-require("./learn_list").run();
-require("./flash_messages").run();    
+document.addEventListener("turbo:load", function () {
+    require("./disable_turbolinks_for_the_forms").run();
+    require("./edit_list").run();
+    require("./learn_list").run();
+    require("./flash_messages").run();  
+});
