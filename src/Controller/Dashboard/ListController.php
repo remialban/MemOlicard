b8c8020dfafa27cf3d10e5f70512eef19ebaa6ed
Rémi ALBAN <remialbanperso@gmail.com>
2022-03-21 18:29:40 +0100

    [feat](list): add a button to delete a list

--- src/Controller/Dashboard/ListController.php
+++ src/Controller/Dashboard/ListController.php
@@ -3,7 +3,10 @@
 namespace App\Controller\Dashboard;
 
 use App\Entity\CardsList;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Lexik\Bundle\JWTAuthenticationBundle\Services\JWTTokenManagerInterface;
@@ -13,15 +16,34 @@ class ListController extends AbstractController
     /**
      * @Route("/dashboard/list/{id}", name="list_view")
      */
-    public function view(CardsList $cardsList, JWTTokenManagerInterface $JWTManager)
+    public function view(CardsList $cardsList,
+        JWTTokenManagerInterface $JWTManager,
+        Request $request,
+        ManagerRegistry $managerRegistry)
     {
         if ($cardsList->getUser() != $this->getUser())
         {
             throw new NotFoundHttpException();
         }
 
+        $removeForm = $this->createFormBuilder()
+            ->getForm()
+        ;
+
+        $removeForm->handleRequest($request);
+
+        if ($removeForm->isSubmitted() && $removeForm->isValid())
+        {
+            $manager = $managerRegistry->getManager();
+            $manager->remove($cardsList);
+            $manager->flush();
+            $this->addFlash("success", "Your list has been deleted");
+            return $this->redirectToRoute("dashboard_home");
+        }
+
         return $this->render('dashboard/lists/view.html.twig', [
             'cardsList' => $cardsList,
+            'removeForm' => $removeForm->createView(),
         ]);
     }
 
